13332b7829ceae940b103b581d5e6a2a98ada0eb
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-02-06 22:49:15 +0200

    sol for ping command

--- app/main.js
+++ app/main.js
@@ -1,11 +1,21 @@
 const net = require("net");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+// console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-//   // Handle connection
+  // Handle connection
+  console.log('client connected');
+  c.on('end', () => {
+    console.log('client disconnected');
+  });
+  c.write("+BONG\r\n");
+  c.pipe(c);
 });
-//
+
+server.on('error', (err) => {
+  throw err;
+});
+
 server.listen(6379, "127.0.0.1");
